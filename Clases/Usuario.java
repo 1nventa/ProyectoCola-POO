35b49ce165585112c22afa457a5be2ea2cd446ac
1nventa <chernandezl1@unicartagena.edu.co>
2024-10-15 21:51:56 -0500

    Añadida clase Main con atributos y métodos

--- Clases/Usuario.java
+++ Clases/Usuario.java
@@ -1,13 +1,10 @@
 public class Usuario {
     private String nombre;
     private String correo;
-    private String contraseña;
-
     
-    public usuario(String nombre, String correo, String contraseña) {
+    public Usuario(String nombre, String correo, String contraseña) {
         this.nombre = nombre;
         this.correo = correo;
-        this.contraseña = contraseña;
     }
 
     
